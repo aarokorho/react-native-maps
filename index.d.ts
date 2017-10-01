0a5edca080697328bdd7ebec66a7969a2a2bc880
Petri Louhelainen <psalmi@iki.fi>
2017-10-01 13:10:53 +0300

    Kamu changes
    Heatmap + support for smartmet coordinates

--- index.d.ts
+++ index.d.ts
@@ -493,6 +493,24 @@ declare module "react-native-maps" {
   }
 
   export class WMSTile extends React.Component<MapWMSTileProps, any> {}
+
+  // =======================================================================
+  //  Heatmap
+  // =======================================================================
+
+  export interface MapHeatMapProps extends ViewProperties {
+    coordinates: {
+      latitude: number,
+      longitude: number,
+      intensity?: number,
+    }[];
+    zIndex?: number;
+    radius?: number;
+    opacity?: number;
+  }
+
+  export class HeatMap extends React.Component<MapHeatMapProps, any> {}
+
   // =======================================================================
   //  Overlay
   // =======================================================================
@@ -504,6 +522,7 @@ declare module "react-native-maps" {
     bounds: [Coordinate, Coordinate];
     tappable?: boolean;
     onPress?: (event: MapEvent<{ action: "overlay-press"; }>) => void;
+    opacity?: number
   }
 
   export class Overlay extends React.Component<MapOverlayProps, any> {}
